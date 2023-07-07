e41f388ad0ec25975828ce3cadd858b7025b1f21
alexanderdiegocarlosvasquez <alexanderdiegocarlosvasquez@gmail.com>
2023-07-07 00:08:08 -0500

    add some things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
+/* eslint-disable semi */
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "server" // default is "dist" for static sites and "server" for SSR sites
 });
---
+++ src/data/books.js
@@ -0,0 +1,28 @@
+/* eslint-disable indent */
+export const books = [
+    {
+        id: 'clean-code',
+        title: 'Clean Code: Manual de estilo para el desarrollo ágil de software',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg'
+    },
+    {
+        id: 'the-clean-coder',
+        title: 'The Clean Coder: A Code of Conduct for Professional Programmers',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1531857377i/39996759.jpg'
+    },
+    {
+        id: 'Rebooting AI',
+        title: 'Rebooting AI: Building Artificial Intelligence We Can Trust',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1555590930i/43999120.jpg'
+    },
+    {
+        id: 'Team Topologies',
+        title: 'Team Topologies: Organizing Business and Technology Teams for Fast Flow',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1563409554i/44135420.jpg'
+    },
+    {
+        id: 'Hello World',
+        title: 'Hello World: Being Human in the Age of Algorithms',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1552089128i/43726517.jpg'
+    }
+]
